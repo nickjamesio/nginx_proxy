5f9409f55203ae8fb9b642c667f2187980defc08
Nick James <thenickjames@gmail.com>
2020-05-21 19:42:05 -0700

    fix blog

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(api.lostcities.nickjames.io www.api.lostcities.nickjames.io nickjames.io www.nickjames.io)
+domains=(nickjames.io www.nickjames.io api.lostcities.nickjames.io www.lostcities.nickjames.io)
 rsa_key_size=4096
 data_path="./certbot"
 email="" # Adding a valid address is strongly recommended
